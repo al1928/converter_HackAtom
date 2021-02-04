4a524a8c93b09abd1e471c609b6b17d876dc6520
miruko-hiro <64906544+miruko-hiro@users.noreply.github.com>
2021-02-04 18:38:24 +0300

    Создан джанго проект

---
+++ server/djangoApi/converter/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ConverterAudioToText(models.Model):
+    url = "/files/test.txt"
---
+++ server/djangoApi/converter/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def getText(request):
+    if request.method == 'POST':
+        url = "/files/test.txt"
+        response = HttpResponse()
+        response['X-Accel-Redirect'] = url
+
+        return response
